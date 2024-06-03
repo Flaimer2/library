ec7f548715c6688e832f8c8be4e986758d51eb45
Flaimer2 <63396386+Flaimer2@users.noreply.github.com>
2024-06-03 10:11:46 +0300

    [1.2] Add Redis Async, Update kotlin to 2.0.0

---
+++ common/src/main/kotlin/RedisExtension.kt
@@ -0,0 +1,17 @@
+package ru.snapix.library
+
+import kotlinx.coroutines.*
+import redis.clients.jedis.Jedis
+
+fun Jedis.useAsync(dispatcher: CoroutineDispatcher = Dispatchers.Main, block: Jedis.() -> Unit) {
+    CoroutineScope(dispatcher).async {
+        block()
+    }
+}
+
+fun <V> Jedis.async(block: Jedis.() -> V): V = runBlocking {
+    val result = async {
+        block()
+    }
+    return@runBlocking result.await()
+}
